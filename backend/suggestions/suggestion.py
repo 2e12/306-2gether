9da4718c659bc9bae7a2670bc7ced5963dbcccc0
2e12 <gabrielbeutler@gmail.com>
2021-05-01 14:11:06 +0200

    Don't suggest users already interacted

--- backend/suggestions/suggestion.py
+++ backend/suggestions/suggestion.py
@@ -1,14 +1,23 @@
+import datetime
+
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
+from backend.suggestions.interactionmodel import Interaction
 from backend.users.useractions import get_users_query
 from backend.users.usermodels import User
 from backend.users.userschemas import UserOutputSchema
 
 
 def get_suggestion(db: Session, user: User) -> UserOutputSchema:
-    users = get_users_query(db)
+    # Filter interacted users
+    start_time = datetime.date.today() - datetime.timedelta(days=2)
+    already_interacted = db.query(Interaction.target).filter(Interaction.user == user.id and Interaction.updated_on >= start_time)
+    users = get_users_query(db).filter(User.id.notin_(already_interacted))
+
+    # Filter myself
     users = users.filter(User.email != user.email)
+
     if users.count() == 0:
         raise HTTPException(status_code=204, detail="No users found")
     first_user = None
