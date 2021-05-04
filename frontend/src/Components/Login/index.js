2a29b6b9429e1bfa96f88f00d1097bf26fe3c7a7
AnnaEmmenegger-boop <anna.emmenegger.xs@gmail.com>
2021-05-04 09:34:16 +0200

    more stuff

--- frontend/src/Components/Login/index.js
+++ frontend/src/Components/Login/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './login.scss';
+import deal_red from "../../assets/navbar/deal_red.png"
 
 function Login() {
 
   return(
-    <div>
-      <p>Login</p>
+    <div className="betterclass">
+        <img src={deal_red} alt="deal_red" className="image"/>
+        <input className="inputField" type="text" placeholder="Email"/>
+        <input className="inputField" type="password" placeholder="Password"/>
     </div>
   )
 }
